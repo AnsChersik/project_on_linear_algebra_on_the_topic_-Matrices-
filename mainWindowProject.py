a9d968991a34d3ff10594d7e2ba518de149e4294
AnsChersik <marschersna@gmail.com>
2025-11-17 21:38:56 +0300

    create element main window

--- mainWindowProject.py
+++ mainWindowProject.py
@@ -1,13 +1,13 @@
 import sys 
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton
 from PyQt6.QtGui import QPixmap
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-
-        self.setGeometry(300, 50, 800, 800)
+        self.setStyleSheet('background-color: #464168')
+        self.setGeometry(300, 50, 800, 650)
         self.setWindowTitle('Главное окно')
         self.initUI()
         self.show()
@@ -22,7 +22,7 @@ class MainWindow(QWidget):
         self.h2LabelMW.move(300, 50)
         self.h2LabelMW.setStyleSheet('color: white; font-size: 18px; font-family: Verdana')
 
-        self.imageCours = './MainWindow_photoes_Matric.jpg'
+        self.imageCours = './photoes/MainWindow_photoes_Matric.jpg'
         self.imageLabel = QLabel(self)
         self.imageLabel.move(190, 100)
         self.imageLabel.setStyleSheet('border: 2px solid black')
@@ -30,13 +30,75 @@ class MainWindow(QWidget):
         self.imagePixmap = QPixmap(self.imageCours)
         self.imageLabel.setPixmap(self.imagePixmap)
 
+        self.pLabelDescPrt1= QLabel('Мы предлагаем вам изучить темы предложенные ниже,', self)
+        self.pLabelDescPrt1.move(150, 300)
+        self.pLabelDescPrt1.setStyleSheet('color: white; font-size: 16px; font-family: Verdana')
+
+        self.pLabelDescPrt2 = QLabel('а по окончанию изучения пройти тест нато как хорошо вы усвоили материал', self)
+        self.pLabelDescPrt2.move(20, 320)
+        self.pLabelDescPrt2.setStyleSheet('color: white; font-size: 16px; font-family: Verdana')
+
+        self.pLabelUl = QLabel('Перечень тем для изучения:', self)
+        self.pLabelUl.move(20, 370)
+        self.pLabelUl.setStyleSheet('color: white; font-size: 16px; font-family: Verdana')
 
 
+        self.pLabelLi1 = QLabel('Матрицы - это...', self)
+        self.pLabelLi1.move(60, 400)
+        self.pLabelLi1.setFixedSize(300, 90)
+        self.pLabelLi1.setStyleSheet('color: white; font-size: 16px; font-family: Verdana; background-color: MediumSlateBlue; padding-left: 5px; border: 5px solid MidnightBlue')
+        self.buttonNewWind1 = QPushButton('Перейти', self)
+        self.buttonNewWind1.move(270, 430)
+        self.buttonNewWind1.setFixedSize(80, 40)
+        self.buttonNewWind1.setStyleSheet('color: white; font-size: 14px; font-family: Verdana; background-color: MidnightBlue')
+        self.buttonNewWind1.clicked.connect(self.OpenWinMatric)
+           
+        self.pLabelLi2 = QLabel('Транспонирование', self)
+        self.pLabelLi2.move(400, 400)
+        self.pLabelLi2.setFixedSize(300, 90)
+        self.pLabelLi2.setStyleSheet('color: white; font-size: 16px; font-family: Verdana; background-color: MediumSlateBlue; padding-left: 5px; border: 5px solid MidnightBlue')
+        self.buttonNewWind2 = QPushButton('Перейти', self)
+        self.buttonNewWind2.move(610, 430)
+        self.buttonNewWind2.setFixedSize(80, 40)
+        self.buttonNewWind2.setStyleSheet('color: white; font-size: 14px; font-family: Verdana; background-color: MidnightBlue')
+        self.buttonNewWind2.clicked.connect(self.OpenWinTransp)
+           
+        self.pLabelLi3 = QLabel('Операции матриц', self)
+        self.pLabelLi3.move(60, 500)
+        self.pLabelLi3.setFixedSize(300, 90)
+        self.pLabelLi3.setStyleSheet('color: white; font-size: 16px; font-family: Verdana; background-color: MediumSlateBlue; padding-left: 5px; border: 5px solid MidnightBlue')
+        self.buttonNewWind3 = QPushButton('Перейти', self)
+        self.buttonNewWind3.move(270, 530)
+        self.buttonNewWind3.setFixedSize(80, 40)
+        self.buttonNewWind3.setStyleSheet('color: white; font-size: 14px; font-family: Verdana; background-color: MidnightBlue')
+        self.buttonNewWind3.clicked.connect(self.OpenWinOper)
+           
+        self.pLabelLi4 = QLabel('Метод Крамера', self)
+        self.pLabelLi4.move(400, 500)
+        self.pLabelLi4.setFixedSize(300, 90)
+        self.pLabelLi4.setStyleSheet('color: white; font-size: 16px; font-family: Verdana; background-color: MediumSlateBlue; padding-left: 5px; border: 5px solid MidnightBlue')
+        self.buttonNewWind4 = QPushButton('Перейти', self)
+        self.buttonNewWind4.move(610, 530)
+        self.buttonNewWind4.setFixedSize(80, 40)
+        self.buttonNewWind4.setStyleSheet('color: white; font-size: 14px; font-family: Verdana; background-color: MidnightBlue')
+        self.buttonNewWind4.clicked.connect(self.OpenWinKramer)
            
 
 
 
 
+    def OpenWinMatric(self):
+        pass
+
+
+    def OpenWinTransp(self):
+        pass
+
+    def OpenWinOper(self):
+        pass
+
+    def OpenWinKramer(self):
+        pass
 
 
 
