884337136cbb4a2b8d5ffc1c5d07062e53014da7
AnsChersik <marschersna@gmail.com>
2025-12-01 23:08:38 +0300

    operations window

--- scr/mainWindowProject.py
+++ scr/mainWindowProject.py
@@ -299,7 +299,7 @@ class WindowOperations(QWidget):
     def __init__(self):
         super().__init__()
         self.setStyleSheet('background-color: #464168')
-        self.setGeometry(300, 50, 800, 650)
+        self.setGeometry(100, 40, 1200, 750)
         self.setWindowTitle('Операции с матрицами')
         self.initUI()
         self.show()
@@ -310,9 +310,60 @@ class WindowOperations(QWidget):
         self.buttonNewWind4.setFixedSize(80, 40)
         self.buttonNewWind4.setStyleSheet('color: white; font-size: 14px; font-family: Verdana; background-color: MidnightBlue')
         self.buttonNewWind4.clicked.connect(self.OpenMainWindow)
-           
+
+        self.h1LabelWO = QLabel('Операции с матрицами', self)
+        self.h1LabelWO.move(450, 10)
+        self.h1LabelWO.setStyleSheet('color: white; font-size: 24px; font-family: Verdana')
+
+        self.pLabelWO1 = QLabel('С матрицами можно выполнять такие операции, как сложение, вычитание и умножение на число.', self)
+        self.pLabelWO1.move(60, 60)
+        self.pLabelWO1.setStyleSheet('color: white; font-size: 20px; font-family: Verdana')
+
+        self.pLabelWO2 = QLabel('Все матрицы можно складывать или вычитать при условии, что они имеют одинаковый размер.', self)
+        self.pLabelWO2.move(60, 120)
+        self.pLabelWO2.setStyleSheet('color: white; font-size: 20px; font-family: Verdana')
+
+        self.pLabelWO2 = QLabel('Для выполнения операций сложения или вычитания необходимо сложить или вычесть соответствующие', self)
+        self.pLabelWO2.move(60, 165)
+        self.pLabelWO2.setStyleSheet('color: white; font-size: 20px; font-family: Verdana')
+
+        self.pLabelWO3 = QLabel('элементы обеих матриц, то есть элементы, расположенные в одинаковых позициях.', self)
+        self.pLabelWO3.move(60, 190)
+        self.pLabelWO3.setStyleSheet('color: white; font-size: 20px; font-family: Verdana')
+
+        self.imageWOMatric = './data/WindowOperations_photoes_OperationSumVich.png'
+        self.imageLabel = QLabel(self)
+        self.imageLabel.move(410, 230)
+        self.imageLabel.setStyleSheet('border: 2px solid black')
+
+        self.imagePixmap = QPixmap(self.imageWOMatric)
+        self.imageLabel.setPixmap(self.imagePixmap)
+
+        self.pLabelWO4 = QLabel('Чтобы умножить матрицу на число, нужно умножить на это число каждый её элемент.', self)
+        self.pLabelWO4.move(60, 475)
+        self.pLabelWO4.setStyleSheet('color: white; font-size: 20px; font-family: Verdana')
+
+        self.imageWOMultiplication = './data/WindowOperations_photoes_OperationUmnNaNum.png'
+        self.imageLabel2 = QLabel(self)
+        self.imageLabel2.move(480, 520)
+        self.imageLabel2.setStyleSheet('border: 2px solid black')
+
+        self.imagePixmap = QPixmap(self.imageWOMultiplication)
+        self.imageLabel2.setPixmap(self.imagePixmap)
+
+        self.buttonTestW3 = QPushButton('Перейти к тесту', self)
+        self.buttonTestW3.move(10, 700)
+        self.buttonTestW3.setFixedSize(150, 40)
+        self.buttonTestW3.setStyleSheet('color: white; font-size: 14px; font-family: Verdana; background-color: MidnightBlue')
+        self.buttonTestW3.clicked.connect(self.OpenTestOper)
+
     def OpenMainWindow(self):
         self.MainWindow = MainWindow()
+        self.MainWindow.show()
+        self.close()
+
+    def OpenTestOper(self):
+        self.MainWindow = WindowMatricesTest()
         self.MainWindow.show()
         self.close()
 
